e874ca059d04d1f7526daf3346bdd6aa36e1f549
vprat <vprat@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-10-19 08:10:03 +0000

    * Removed view link for files * Added icon for unread messages in conversation
    git-svn-id: https://plugins.svn.wordpress.org/customer-area/trunk@790333 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- includes/core-addons/private-file/templates/private-file-after_post_content.template.php
+++ includes/core-addons/private-file/templates/private-file-after_post_content.template.php
@@ -7,11 +7,7 @@
 			<td class="title">
 				<?php CUAR_PrivateFileThemeUtils::the_file_name( get_the_ID() ); ?>
 			</td>
-			<td class="view-link">
-				<a href="<?php CUAR_PrivateFileThemeUtils::the_file_link( get_the_ID(), 'view' ); ?>" title="<?php esc_attr_e( 'View', 'cuar' ); ?>">	
-					<?php _e( 'View', 'cuar' ); ?></a>
-			</td> 
-			<td class="download-link">
+			<td class="links download-link">
 				<a href="<?php CUAR_PrivateFileThemeUtils::the_file_link( get_the_ID(), 'download' ); ?>" title="<?php esc_attr_e( 'Download', 'cuar' ); ?>">
 					<?php _e( 'Download', 'cuar' ); ?></a>
 			</td>
